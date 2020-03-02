08dc90ebe6c587033eeab411a3f11fccfbc32965
Jon Gjengset <jon@thesquareplanet.com>
2020-03-02 14:41:29 -0500

    Add HashMap::remove_entry and HashSet::take

--- src/set.rs
+++ src/set.rs
@@ -306,6 +306,33 @@ where
         let removed = self.map.remove(value, guard);
         removed.is_some()
     }
+
+    /// Removes and returns the value in the set, if any, that is equal to the given one.
+    ///
+    /// The value may be any borrowed form of the set's value type, but
+    /// [`Hash`] and [`Eq`] on the borrowed form *must* match those for
+    /// the value type.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use flurry::HashSet;
+    ///
+    /// let mut set: HashSet<_> = [1, 2, 3].iter().cloned().collect();
+    /// let guard = set.guard();
+    /// assert_eq!(set.take(&2, &guard), Some(&2));
+    /// assert_eq!(set.take(&2, &guard), None);
+    /// ```
+    ///
+    /// [`Eq`]: ../../std/cmp/trait.Eq.html
+    /// [`Hash`]: ../../std/hash/trait.Hash.html
+    pub fn take<'g, Q>(&'g self, value: &Q, guard: &'g Guard) -> Option<&'g T>
+    where
+        T: Borrow<Q>,
+        Q: ?Sized + Hash + Eq,
+    {
+        self.map.remove_entry(value, guard).map(|(k, _)| k)
+    }
 }
 
 impl<T, S> PartialEq for HashSet<T, S>
